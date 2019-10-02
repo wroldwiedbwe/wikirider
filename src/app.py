09993fba3acc5c0b066dda8705809fd62534be56
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2019-10-02 09:51:42 +0200

    Updated Argument Parsing
    Also fixed indentation

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from __future__ import print_function, absolute_import
 from sys import argv as args
 from src.wikirider import WikiRider
 from src.view import RidePrinter
+import argparse
 
 
 class TerminalApp:
@@ -10,30 +11,31 @@ class TerminalApp:
 
     def __init__(self):
         self.printer = RidePrinter()
-        self.args = args
+        self.pargs = self.parse_arguments()
 
     def start(self):
         self.printer.print_banner()
         if self._should_start():
-            rider = WikiRider(self.args[1], int(self.args[2]))
+            rider = WikiRider(self.pargs.url, self.pargs.depth)
             self.printer.print_start()
             for rider_state in rider.run():
                 self.printer.print_rider_location(rider_state)
             self.printer.print_end()
 
     def _should_start(self):
-        if len(self.args) != 3:
-            self.printer.print_help()
-            return False
-        if not WikiRider.valid_url(self.args[1]) or not valid_int(self.args[2]):
+        if not WikiRider.valid_url(self.pargs.url):
             self.printer.print_invalid_input_error()
             return False
         return True
 
-
-def valid_int(num):
-    try:
-        int(num)
-        return True
-    except ValueError:
-        return False
+    @staticmethod
+    def parse_arguments():
+        parser = argparse.ArgumentParser(
+            description='Performs a wiki run on the provided url. The length of the run can be adjusted by providing a depth argument')
+        parser.add_argument('url', metavar='url', type=str,
+                            help='The wiki url that should be used')
+        parser.add_argument('depth', metavar='depth',
+                            type=int, default=5, help='The recursion depth')
+        args = parser.parse_args()
+        
+        return args
