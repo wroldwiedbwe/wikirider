6787b0f2676ab5e260421edad229e7412469d91c
DasSinaTier <info@dassinatier.de>
2019-10-13 20:13:38 +0200

    add check if pip is installed

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 echo -e "\e[1;32mInstalling wikirider...\e[0m"
 
+command -v pip >/dev/null 2>&1 || { echo >&2 "I require pip but it's not installed.  Aborting."; exit 1; }
 if pip install virtualenv && virtualenv .env && source $(pwd)/.env/bin/activate && pip install -r requirements.txt;
 then
     echo -e "\e[1;32mInstallation complete!"
