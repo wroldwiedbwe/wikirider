e2a16840478042cd28cf2b48442da1c23ddcb9bf
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2019-10-30 21:34:59 +0100

    Upgrade setup script

--- setup.sh
+++ setup.sh
@@ -2,18 +2,33 @@
 
 echo -e "\e[1;32mInstalling wikirider...\e[0m"
 
+PYVER=$(python -V 2>&1 | grep -Po '(?<=Python )(\d)')
 CUR_DIR=$(pwd)
+PY="python"
+
+if [[ `command -v python3` ]]; then
+    export PYVER="3";
+    export PY="python3";
+fi
+
+if [[ $PYVER -ne "3" ]]; then
+    echo -e "\e[1;31mOnly Python 3.x is supported! Make sure $(command -v python) links to Python 3.x\e[0m";
+    exit;
+fi
 
 # if an arg is passed, use this as install location
 if  [ "$1" ] ; then
     INSTALL_LOC=$1
     rsync -a --exclude=.* $CUR_DIR $INSTALL_LOC
 else
-    set INSTALL_LOC=$CUR_DIR
+    export INSTALL_LOC=$CUR_DIR
 fi
 
-FULL_LOC=$INSTALL_LOC/wikirider
-cd $FULL_LOC
+export FULL_LOC="$INSTALL_LOC/wikirider"
+export OLD_LOC="$CUR_DIR"
+rm -rf "$FULL_LOC"
+mkdir -p "$FULL_LOC"
+cd "$FULL_LOC"
 
 echo -e "\e[1;32mInstall location is $FULL_LOC"
 
@@ -26,11 +41,13 @@ else
     exit 1
 fi
 
-if $PIP install virtualenv && virtualenv .env && source $(pwd)/.env/bin/activate && $PIP install -r requirements.txt; then
+export CUR_DIR=$(pwd)
+
+if $PIP install virtualenv && virtualenv .env -p $PY && source "$CUR_DIR/.env/bin/activate" && $PIP install -r "$OLD_LOC/requirements.txt"; then
     echo -e "\e[1;32mInstallation complete!"
     echo -e "To run wikirider, do \e[0;33m'cd $FULL_LOC; source .env/bin/activate && python wikirun.py'\e[1;32m!"
     echo -e "Don't forget to do \e[0;33m'deactivate'\e[1;32m after you're done to exit the virtualenv!\e[0m"
 else
     echo -e "\e[1;31mSome errors occured! Look up to see what went wrong.\e[0m"
 fi
-cd $CUR_DIR
+cd "$CUR_DIR"
